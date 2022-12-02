672b3b21d3ece793f4285b5ce96c2b07bc1de45c
Pierre Stridsberg <pierre.stridsberg@astrazeneca.com>
2022-12-02 09:42:19 +0100

    Day2 refactor using dicts

--- day02/aoc.py
+++ day02/aoc.py
@@ -2,87 +2,52 @@ from functools import reduce
 from operator import add
 from os import environ
 
-LOST=0
-DRAW=3
-WON=6
-ROCK_P=1
-PAPER_P=2
-SCISSORS_P=3
+scores_map = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
+}
 
-OP_R_M="A"
-OP_P_M="B"
-OP_S_M="C"
-
-ME_R_M="X"
-ME_P_M="Y"
-ME_S_M="Z"
-
-SHOULD_LOOSE = "X"
-S_DRAW = "Y"
-WIN = "Z"
+actual_scores_map = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7
+}
 
 
 def getSolutionPart1(input_list):
-    #
+    #13682
     my_scores = []
     s = [x.split(" ") for x in [y.replace("\n", '') for y in input_list]]
     for moves in s:
-        op_move = moves[0]
-        me_move = moves[1]
-        if op_move == OP_R_M and me_move != ME_P_M and me_move != ME_R_M:
-            my_scores.append(SCISSORS_P + LOST)
-        elif op_move == OP_R_M and me_move == ME_P_M:
-            my_scores.append(PAPER_P + WON)
-        elif op_move == OP_R_M and me_move == ME_R_M:
-            my_scores.append(ROCK_P + DRAW)
-        elif op_move == OP_P_M and me_move != ME_P_M and me_move != ME_S_M:
-            my_scores.append(ROCK_P + LOST)
-        elif op_move == OP_P_M and me_move == ME_S_M:
-            my_scores.append(SCISSORS_P + WON)
-        elif op_move == OP_P_M and me_move == ME_P_M:
-            my_scores.append(PAPER_P + DRAW)
-        elif op_move == OP_S_M and me_move != ME_R_M and me_move != ME_S_M:
-            my_scores.append(PAPER_P + LOST)
-        elif op_move == OP_S_M and me_move == ME_R_M:
-            my_scores.append(ROCK_P + WON)
-        elif op_move == OP_S_M and me_move == ME_S_M:
-            my_scores.append(SCISSORS_P + DRAW)
-
+        my_scores.append(scores_map.get(f"{moves[0]} {moves[1]}"))
     return reduce(add, my_scores)
 
 
 def getSolutionPart2(input_list):
-    #
+    #12881
     my_scores = []
     s = [x.split(" ") for x in [y.replace("\n", '') for y in input_list]]
     for moves in s:
-        op_move = moves[0]
-        outcome = moves[1]
-        if op_move == OP_R_M and outcome != WIN and outcome != S_DRAW:
-            my_scores.append(SCISSORS_P + LOST)
-        elif op_move == OP_R_M and outcome == WIN:
-            my_scores.append(PAPER_P + WON)
-        elif op_move == OP_R_M and outcome == S_DRAW:
-            my_scores.append(ROCK_P + DRAW)
-        elif op_move == OP_P_M and outcome != WIN and outcome != S_DRAW:
-            my_scores.append(ROCK_P + LOST)
-        elif op_move == OP_P_M and outcome == WIN:
-            my_scores.append(SCISSORS_P + WON)
-        elif op_move == OP_P_M and outcome == S_DRAW:
-            my_scores.append(PAPER_P + DRAW)
-        elif op_move == OP_S_M and outcome != WIN and outcome != S_DRAW:
-            my_scores.append(PAPER_P + LOST)
-        elif op_move == OP_S_M and outcome == WIN:
-            my_scores.append(ROCK_P + WON)
-        elif op_move == OP_S_M and outcome == S_DRAW:
-            my_scores.append(SCISSORS_P + DRAW)
+        my_scores.append(actual_scores_map.get(f"{moves[0]} {moves[1]}"))
     return reduce(add, my_scores)
 
 
 with open('input.txt') as f:
     file_input = [x for x in f.readlines()]
 
-print('Python')
 part = environ.get('part')
 
 if part == 'part2':
