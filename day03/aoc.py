0a59a70346992211b2701548d03437db000e72a6
Pierre Stridsberg <pierre.stridsberg@astrazeneca.com>
2022-12-03 18:42:31 +0100

    Day 3 python fewer lines

--- day03/aoc.py
+++ day03/aoc.py
@@ -3,46 +3,26 @@ from functools import reduce
 from operator import add
 from os import environ
 
-priority_map = {}
-
 all_chars_list = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 prio_list = [i for i in range(1, 53)]
-
-for i in range(len(prio_list)):
-    priority_map[all_chars_list[i]] = prio_list[i]
+priority_map = {all_chars_list[i]: prio_list[i] for i in range(len(prio_list))}
 
 
 def get_compartments(data):
-    x = len(data)
-    return [data[slice(0, x // 2)], data[slice(x // 2, x)]]
-
-
-def chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+    return [data[slice(0, len(data) // 2)], data[slice(len(data) // 2, len(data))]]
 
 
 def getSolutionPart1(input_list):
     # 7674
-    compartments_list = [get_compartments(data) for data in input_list]
-    prio_value_list = []
-    for comp_list in compartments_list:
-        a = list(set(comp_list[0]) & set(comp_list[1]))
-        for s in a:
-            prio_value_list.append(priority_map.get(s))
-
-    return reduce(add, prio_value_list)
+    return reduce(add, [priority_map.get(s[0]) for s in [list(set(comp_list[0]) & set(comp_list[1])) for comp_list in
+                                                         [get_compartments(data) for data in input_list]]])
 
 
 def getSolutionPart2(input_list):
     # 2805
-    prio_value_list = []
-    elf_groups = list(chunks(input_list, 3))
-    for group in elf_groups:
-        a = list(set(group[0]) & set(group[1]) & set(group[2]))
-        for x in a:
-            prio_value_list.append(priority_map.get(x))
-    return reduce(add, prio_value_list)
+    return reduce(add, [priority_map.get(s[0]) for s in
+                        [list(set(group[0]) & set(group[1]) & set(group[2])) for group in
+                         [input_list[i:i + 3] for i in range(0, len(input_list), 3)]]])
 
 
 with open('input.txt') as f:
