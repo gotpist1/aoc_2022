7214c7f43b5ab4b85f40ebd016a5e0d6f792f80b
Pierre Stridsberg <pierre.stridsberg@astrazeneca.com>
2022-12-04 15:33:29 +0100

    Day 4 python fewer lines

--- day04/aoc.py
+++ day04/aoc.py
@@ -3,34 +3,25 @@ from operator import add
 from os import environ
 
 
-def getSolutionPart1(input_list):
-    # 475
-    fully_contains_count = []
-    s = [section_pairs.split(",") for section_pairs in input_list]
-    for d in s:
+def get_range_arrays(sections):
+    for d in sections:
         fnum = [x for x in range(int(d[0].split("-")[0]), int(d[0].split("-")[1]) + 1)]
         snum = [x for x in range(int(d[1].split("-")[0]), int(d[1].split("-")[1]) + 1)]
-        result_all = all(elem in fnum for elem in snum)
-        result_all_2 = all(elem in snum for elem in fnum)
-        if result_all or result_all_2:
-            fully_contains_count.append(True)
+        yield fnum, snum
 
+def getSolutionPart1(input_list):
+    # 475
+    sections = [section_pairs.split(",") for section_pairs in input_list]
+    ranged_arrays = list(get_range_arrays(sections))
+    fully_contains_count = [True for fnum, snum in ranged_arrays if all(elem in fnum for elem in snum) or all(elem in snum for elem in fnum)]
     return reduce(add, fully_contains_count)
 
 
 def getSolutionPart2(input_list):
     # 825
-    overlaps = []
-    s = [section_pairs.split(",") for section_pairs in input_list]
-    for d in s:
-        first = d[0].split("-")
-        second = d[1].split("-")
-        fnum = [x for x in range(int(first[0]), int(first[1]) + 1)]
-        snum = [x for x in range(int(second[0]), int(second[1]) + 1)]
-        result_all = any(elem in fnum for elem in snum)
-        if result_all:
-            overlaps.append(True)
-
+    sections = [section_pairs.split(",") for section_pairs in input_list]
+    ranged_arrays = list(get_range_arrays(sections))
+    overlaps = [True for fnum, snum in ranged_arrays if any(elem in fnum for elem in snum)]
     return reduce(add, overlaps)
 
 
