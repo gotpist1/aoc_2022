32278bd6248b3b0dbcceeb085a19c68a95ea0890
Pierre Stridsberg <pierre.stridsberg@astrazeneca.com>
2022-12-06 12:11:20 +0100

    Day 6 python fewer lines

--- day06/aoc.py
+++ day06/aoc.py
@@ -1,29 +1,17 @@
 from os import environ
 
 
-def get_key_marker(marker_length, input_file):
-    for i in range(0, len(input_file)):
-        key = input_file[i:i + marker_length]
-        if len(set(key)) == len(key):
-            return i + len(key)
-
-
-def getSolutionPart1(input_list):
-    # 1198
-    return get_key_marker(4, input_list)
-
-
-def getSolutionPart2(input_list):
-    # 3120
-    return get_key_marker(14, input_list)
+def get_key_marker(n, input_file):
+    return [i + n for i in range(0, len(input_file)) if len(set(input_file[i:i + n])) == n][0]
 
 
 with open('input.txt') as f:
     input_buffer = f.read()
 
 part = environ.get('part')
-
 if part == 'part2':
-    print(getSolutionPart2(input_buffer))
+    # 3120
+    print(get_key_marker(14, input_buffer))
 else:
-    print(getSolutionPart1(input_buffer))
+    # 1198
+    print(get_key_marker(4, input_buffer))
